6eea62c03d1f055fd77916bd32dd38fe747149cf
Chris Quarshie <chrisquarshie57@gmail.com>
2020-06-01 19:36:37 +0000

    second commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
-let d = new Date();
-document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+let name = prompt("What is your name?");
+let age = prompt("What is your age?");
+let colour = prompt("What is your favourite colour?");
+
+alert("Hello, " + name + "!" + " If you are " + age + " years old then you're old and your favourite colour " + colour + " is my favourite." );
